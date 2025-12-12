e839e69ff2edc2b29cb347b161c3d63d70242b46
Davit-Tbileli <datunatbileli@gmail.com>
2025-12-12 15:49:57 +0400

    Snapshot commit: save current workspace (do not delete files)

--- llm/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-LLM integration module for conversational recommendations.
-"""
-
-from .prompts import PromptTemplates
-from .chatbot import BOGChatbot
-
-__all__ = ['PromptTemplates', 'BOGChatbot']
